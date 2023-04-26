af31fb3142d1b3cf30b6bacdaf22cdbca2901737
MuhammadSoliman14 <musoliman14@gmail.com>
2023-04-26 20:04:41 +0200

    Small fixes in PlanTrip.html

--- scripts/map.js
+++ scripts/map.js
@@ -15,6 +15,13 @@
     showRoute5();
     
     }
+
+
+
+
+
+
+
       
       function showRoute1(){
         var map = new google.maps.Map(document.getElementById('map'), {
@@ -125,102 +132,69 @@
     
     
     
-      function showRoute2() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+      function showRoute4(){
+  var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 30.8201, lng: 28.9526},
     zoom: 10
     });
-    
-    var Routelocations = [
-    { name: "Roushdy", lat: 31.22793103506819, lng: 29.95176114876179},          
-    // { name: "Sedi Gaber", lat: 31.221558760235006, lng: 29.966225859271766 },
-    { name: "Sporting", lat: 31.21235666850399, lng: 29.93411308700536 },  
-    { name: "Ibrahimia", lat: 31.208225508358357, lng: 29.929442942494376 },
-    { name: "AIU", lat: 30.811834913805104, lng: 28.929844683814757 }
-    ];
-    
-    var latLngArray = locations.map(function(location) {
+  
+  
+    var route3locations = [
+      { name: "Roushdy", lat: 31.22793103506819, lng: 29.95176114876179},          
+      // { name: "Sedi Gaber", lat: 31.221558760235006, lng: 29.966225859271766 },
+      { name: "Sporting", lat: 31.21235666850399, lng: 29.93411308700536 },  
+      { name: "Ibrahimia", lat: 31.208225508358357, lng: 29.929442942494376 },
+      { name: "AIU", lat: 30.811834913805104, lng: 28.929844683814757 }
+      ];
+      
+  var latLngArray3 = route3Locations.map(function(location) {
     return new google.maps.LatLng(location.lat, location.lng);
-    });
-    
-    var directionsService = new google.maps.DirectionsService();
-    
-    var waypoints = locations.slice(1, locations.length - 1).map(function(location) {
+  });
+  
+  var waypoints3 = route3Locations.slice(1, route3Locations.length - 1).map(function(location) {
     return { location: { lat: location.lat, lng: location.lng } };
-    });
-    
-    var request = {
-    origin: latLngArray[0],
-    destination: latLngArray[latLngArray.length - 1],
-    waypoints: waypoints,
+  });
+  
+  var request3 = {
+    origin: latLngArray3[0],
+    destination: latLngArray3[latLngArray3.length - 1],
+    waypoints: waypoints3,
     optimizeWaypoints: true,
     travelMode: google.maps.TravelMode.DRIVING
-    };
-    
-    
-    directionsService.route(request, function(response, status) {
+  };
+  
+  var directionsService = new google.maps.DirectionsService();
+  
+  directionsService.route(request3, function(response, status) {
     if (status === google.maps.DirectionsStatus.OK) {
-      console.log(response); // log the response object for debugging
-    
-    
-    
-      var directionsRenderer = new google.maps.DirectionsRenderer({
+      var directionsRenderer3 = new google.maps.DirectionsRenderer({
         map: map,
-        suppressMarkers: true
-      });
-      directionsRenderer.setDirections(response);
-    
-      // Add an event listener to display info window on route click
-      google.maps.event.addListener(directionsRenderer, 'click', function(event) {
-        // Get the location clicked on the route
-        var location = event.latLng;
-    
-        // Find the closest location in the locations array
-        var closestLocation = getClosestLocation(location, locations);
-    
-        // Create the content for the info window
-        var content = '<div><strong>' + closestLocation.name + '</strong></div>';
-    
-        // Create the info window
-        var infoWindow = new google.maps.InfoWindow({
-          content: content
-        });
-    
-        // Open the info window at the clicked location
-        infoWindow.setPosition(location);
-        infoWindow.open(map);
-      });
-    
-      function getClosestLocation(location, locations) {
-        var closestLocation = null;
-        var closestDistance = Infinity;
-    
-        for (var i = 0; i < locations.length; i++) {
-          var distance = google.maps.geometry.spherical.computeDistanceBetween(location, new google.maps.LatLng(locations[i].lat, locations[i].lng));
-          if (distance < closestDistance) {
-            closestLocation = locations[i];
-            closestDistance = distance;
-          }
+        suppressMarkers: true,
+        polylineOptions: {
+          strokeColor: "green" // Replace with your desired color
         }
-    
-        return closestLocation;
-      }
-    
-    
+      });
+      directionsRenderer3.setDirections(response);
     } else {
       console.error('Directions request failed due to ' + status);
     }
-    });
-    
-    locations.forEach(function(location) {
+  });
+  
+  route3Locations.forEach(function(location) {
     var marker = new google.maps.Marker({
       position: { lat: location.lat, lng: location.lng },
       map: map,
       title: location.name
-    
     });
-    });
-      }
+  });
+  
+  
+  }
+  
+  
+  
+  
+
 
 
       // var route3Locations = [
